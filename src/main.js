c64e3913c41ead958c40f2432c87760adc9927bc
Alex Berk <aleks.berkov@gmail.com>
2026-04-15 21:05:18 +0300

    switch font to space-grotesk

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import '@fontsource/space-grotesk/400.css';
+import '@fontsource/space-grotesk/700.css';
 import './styles.css';
 
 createApp(App).mount('#root');
